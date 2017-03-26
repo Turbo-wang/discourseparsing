43c713a73892997dc4be0e03f1c982c113e40fc2
Turbo-wangh <wanghaotaiyuan@163.com>
2017-03-27 01:42:52 +0800

    @wanghao commit for a single model

--- SimpleLSTM.py
+++ SimpleLSTM.py
@@ -2,16 +2,16 @@ import tensorflow as tf
 import numpy
 
 class SimpleLSTM:
-    def __init__(self, max_len=50, hidden_layer=1280, num_classes=2, embedding_matrix, sequence_length):
+    def __init__(self, max_len=50, hidden_layer=1280, num_classes=2, sequence_length):
         self.sequence_length = sequence_length
         self.hidden_layer = hidden_layer
         self.n_class = num_classes
         # Placeholders for input, output and dropout
-        self.x1_indexs = tf.placeholder(tf.int32, [None, sequence_length], name="x1_indexs")
-        self.x1_pos = tf.placeholder(tf.float32, [None, sequence_length, 46], name="x1_pos")
+        # self.x1_indexs = tf.placeholder(tf.int32, [None, sequence_length], name="x1_indexs")
+        self.x1_inputs = tf.placeholder(tf.float32, [None, sequence_length, 46], name="x1_pos")
 
-        self.x2_indexs = tf.placeholder(tf.int32, [None, sequence_length], name="x2_indexs")
-        self.x2_pos = tf.placeholder(tf.float32, [None, sequence_length, 46], name="x2_pos")
+        # self.x2_indexs = tf.placeholder(tf.int32, [None, sequence_length], name="x2_indexs")
+        self.x2_inputs = tf.placeholder(tf.float32, [None, sequence_length, 46], name="x2_pos")
         self.y = tf.placeholder(tf.float32, [None, num_classes], name="y")
         # self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
         # self.lr = tf.placeholder(tf.float32, name="learning_rate")
@@ -19,17 +19,17 @@ class SimpleLSTM:
 
         l2_loss = tf.constant(0.0)
         # Embedding layer
-        with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            normal_embedding_matrix = tf.nn.l2_normalize(embedding_matrix, dim=1)
+        # with tf.device('/cpu:0'), tf.name_scope("embedding"):
+        #     normal_embedding_matrix = tf.nn.l2_normalize(embedding_matrix, dim=1)
 
-            self.W = tf.Variable(normal_embedding_matrix, name="W_embedding")
-            self.embedded_chars1 = tf.nn.embedding_lookup(self.W, self.x1_indexs)
-            self.em_other1 = tf.concat(2, [self.embedded_chars, self.x1_pos])
-            self.embedded_chars_expanded1 = tf.expand_dims(self.em_other1, -1)
+        #     self.W = tf.Variable(normal_embedding_matrix, name="W_embedding")
+        #     self.embedded_chars1 = tf.nn.embedding_lookup(self.W, self.x1_indexs)
+        #     self.em_other1 = tf.concat(2, [self.embedded_chars, self.x1_pos])
+        #     self.embedded_chars_expanded1 = tf.expand_dims(self.em_other1, -1)
 
-            self.embedded_chars2 = tf.nn.embedding_lookup(self.W, self.x2_indexs)
-            self.em_other2 = tf.concat(2, [self.embedded_chars, self.x2_pos])
-            self.embedded_chars_expanded2 = tf.expand_dims(self.em_other2, -1)
+        #     self.embedded_chars2 = tf.nn.embedding_lookup(self.W, self.x2_indexs)
+        #     self.em_other2 = tf.concat(2, [self.embedded_chars, self.x2_pos])
+        #     self.embedded_chars_expanded2 = tf.expand_dims(self.em_other2, -1)
         weights = {
                    'out': tf.Variable(tf.random_normal([self.n_hidden, self.n_class]))
                }
@@ -43,7 +43,7 @@ class SimpleLSTM:
         # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
 
         # Permuting batch_size and n_steps
-        x1 = tf.transpose(self.embedded_chars_expanded1, [1, 0, 2])
+        x1 = tf.transpose(self.x1_inputs, [1, 0, 2])
         # Reshaping to (n_steps*batch_size, n_input)
         x1 = tf.reshape(x1, [-1, self.sequence_length])
         # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
@@ -55,7 +55,7 @@ class SimpleLSTM:
         # Get lstm cell output
         outputs1, states1 = rnn.static_rnn(lstm_cell, x1, dtype=tf.float32)
 
-        x2 = tf.transpose(self.embedded_chars_expanded2, [1, 0, 2])
+        x2 = tf.transpose(self.x2_inputs, [1, 0, 2])
         # Reshaping to (n_steps*batch_size, n_input)
         x2 = tf.reshape(x2, [-1, self.sequence_length])
         # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
@@ -68,6 +68,16 @@ class SimpleLSTM:
         outputs2, states2 = rnn.static_rnn(lstm_cell, x2, dtype=tf.float32)
 
 
-        
+        W1 = tf.Variable(tf.random_normal([self.n_hidden, self.n_hidden]))
+        W2 = tf.Variable(tf.random_normal([self.n_hidden, self.n_hidden]))
+
+        o1 = tf.matmul(tf.matmul(outputs1[-1], W1), tf.transpose(outputs2[-1]))
+        o2 = tf.matmul(tf.matmul(outputs2[-1], W2), tf.transpose(outputs2[-1]))
         # Linear activation, using rnn inner loop last output
-        return tf.matmul(outputs[-1], weights['out']) + biases['out']
+        if (o1-o2>0.3):
+            res = np.array([1, 0], dtype="float32")
+        else:
+            res = np.array([0, 1], dtype="float32")
+        losses = tf.nn.softmax_cross_entropy_with_logits(res, self.y)
+        self.loss = tf.reduce_mean(losses)
+        self.optimizer = tf.train.AdagradOptimizer(learning_rate=0.001).minimize(self.loss)
--- data_utils.py
+++ data_utils.py
@@ -6,12 +6,73 @@ class DataUtil:
 
     def get_sentence_pairs(self, filename, filepath):
         self.json_list = []
+        self.sentence_list1 = []
+        self.sentence_list2 = []
+        self.pos_list1 = []
+        self.pos_list2 = []
         with open(os.path.join(filepath, filename)) as f:
             for line in f:
                 jsonobj = json.loads(line.strip())
+                arg1 = json_obj["Arg1"]
+                arg2 = json_obj["Arg2"]
+                pos_list1 = arg1["POS"]
+                pos_list2 = arg2["POS"]
+                sen_list1 = arg1["Word"]
+                sen_list2 = arg2["Word"]
+                connect = ar
+                self.sentence_list1.append(sen_list1)
+                self.sentence_list2.append(sen_list2)
+                self.pos_list1.append(pos_list1)
+                self.pos_list2.append(pos_list2)
+                self.relation = 
+
             # print json.dumps(jsonobj, indent=2)
             # break
-                self.json_list.append(jsonobj)
+                # self.json_list.append(jsonobj)
+
+    def get_train_data():
+        self.load_word2vec()
+        self.get_sentence_pairs()
+        self.batch_size = 128
+        X1_inputs_list1 = []
+        X2_inputs_list2 = []
+        Y_inputs_list = []
+        for sen1, sen2 in zip(self.sentence_list1, self.sentence_list2):
+            X1_inputs_batch = []
+            X2_inputs_batch = []
+            Y_inputs_batch = []
+            x1_sen = []
+            flag = False
+            for idx, word in enumerate(sen1):
+                if idx==50:
+                    break
+                    flag = True
+                if word in self.model:
+                    x1_sen.append(self.model[word])
+                else:
+                    x1_sen.append(np.zeros(300), dtype="float32")
+            if not flag:
+                for i in range(len(x1_sen), 50):
+                    x1_sen.append(np.zeros(300), dtype="float32")
+            X1_inputs_batch.append(x1_sen)
+            x2_sen = []
+            flag = False
+            for idx, word in enumerate(sen2):
+                if idx==50:
+                    break
+                    flag = True
+                if word in self.model:
+                    x2_sen.append(self.model[word])
+                else:
+                    x2_sen.append(np.zeros(300), dtype="float32")
+            if not flag:
+                for i in range(len(x2_sen), 50):
+                    x2_sen.append(np.zeros(300), dtype="float32")
+            X2_inputs_batch.append(x2_sen)
+
+
+
+
 
     def load_word2vec(self, file_name):
         self.model = gensim.models.Word2Vec.load_word2vec_format(file_name, binary=True)
